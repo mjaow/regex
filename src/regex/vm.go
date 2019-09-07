e392e7200ac6192ecdde768802dc65259c142bad
mjaow <mjaow0128@gmail.com>
2019-09-08 02:01:24 +0800

    init vm

---
+++ src/regex/vm.go
@@ -0,0 +1,14 @@
+package regex
+
+type instructor struct {
+}
+
+func ThompsonVM(target string, instructors []instructor) bool {
+	//TODO thompson vm implementation
+	return true
+}
+
+func PikeVM(target string, instructors []instructor) bool {
+	//TODO pike vm implementation
+	return true
+}
